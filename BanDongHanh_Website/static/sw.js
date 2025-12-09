4d5fee965c3eb511386e454831900bdd2dfc0243
Wateristhat <vuvanhienbb25@gmail.com>
2025-12-09 14:04:51 +0700

    Replace JSON manifest with service worker logic

---
+++ BanDongHanh_Website/static/sw.js
@@ -0,0 +1,18 @@
+// static/sw.js
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open('ban-dong-hanh-v1').then(function(cache) {
+            return cache.addAll([
+                '/'
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            return response || fetch(event.request);
+        })
+    );
+});
